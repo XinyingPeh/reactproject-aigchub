6a4e4f7794500faa34b29e021c31d625d4f33ab1
Xinying Peh <jupitoria@gmail.com>
2023-08-06 11:03:33 +0800

    format doc & update npm

--- src/components/Firebase.js
+++ src/components/Firebase.js
@@ -1,25 +1,28 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from 'firebase/firestore';
 
-const apikeyNum = process.env.REACT_APP_API_KEY_FIREBASE
-const authDomain = process.env.REACT_APP_AUTHDOMAIN_FIREBASE
-const projectId = process.env.REACT_APP_PROJECTID_FIREBASE
-const storageBucket = process.env.REACT_APP_STORAGEBUCKET_FIREBASE
-const messagingSenderId = process.env.REACT_APP_MESSAGINGSENDERID_FIREBASE
-const appId = process.env.REACT_APP_APPID_FIREBASE
+const apikeyNum = process.env.REACT_APP_API_KEY_FIREBASE;
+const authDomain = process.env.REACT_APP_AUTHDOMAIN_FIREBASE;
+const projectId = process.env.REACT_APP_PROJECTID_FIREBASE;
+const storageBucket = process.env.REACT_APP_STORAGEBUCKET_FIREBASE;
+const messagingSenderId = process.env.REACT_APP_MESSAGINGSENDERID_FIREBASE;
+const appId = process.env.REACT_APP_APPID_FIREBASE;
 
 const firebaseConfig = {
-    apiKey: apikeyNum,
-    authDomain: authDomain,
-    projectId: projectId,
-    storageBucket: storageBucket,
-    messagingSenderId: messagingSenderId,
-    appId: appId
-  };
+  apiKey: apikeyNum,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId: messagingSenderId,
+  appId: appId,
+};
 
-  
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
 
-  export { db };
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
 
+export { db };
